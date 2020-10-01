d5168217d3ad8d2a4bf7c2f228bf010a0cc0a7f4
AJ <1018212@student.sae.edu.au>
2020-10-01 12:52:38 +1000

    Added namespace

--- Assets/Team members/Alex B/Scripts/AI scripts/AIMoveforward.cs
+++ Assets/Team members/Alex B/Scripts/AI scripts/AIMoveforward.cs
@@ -4,45 +4,49 @@ using Mirror.Examples.RigidbodyPhysics;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AIMoveforward : MonoBehaviour
+namespace AJ
 {
-    public Transform[] waypoints;
-    public int speed;
+    public class AIMoveforward : MonoBehaviour
+    {
+        public Transform[] waypoints;
+        public int speed;
     
 
-    private int waypointIndex;
-    private float dist;
-    void Start()
-    {
-        waypointIndex = 0;
-        transform.LookAt(waypoints[waypointIndex].position);
-    }
-
-    
-    void Update()
-    {
-        dist = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
-        if (dist < 1f)
-        {
-            IncreaseIndex();
-        }
-        Patrol();
-    }
-
-    void Patrol()
-    {
-        GetComponent<Rigidbody>().AddForce(transform.forward*3);
-        
-        //(Vector3.forward * speed * Time.deltaTime);
-    }
-
-    void IncreaseIndex()
-    {
-        waypointIndex++;
-        if (waypointIndex >= waypoints.Length)
+        private int waypointIndex;
+        private float dist;
+        void Start()
         {
             waypointIndex = 0;
+            transform.LookAt(waypoints[waypointIndex].position);
         }
-        transform.LookAt(waypoints[waypointIndex].position);
+
+    
+        void Update()
+        {
+            dist = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
+            if (dist < 1f)
+            {
+                IncreaseIndex();
+            }
+            Patrol();
+        }
+
+        void Patrol()
+        {
+            GetComponent<Rigidbody>().AddForce(transform.forward*3);
+        
+            //(Vector3.forward * speed * Time.deltaTime);
+        }
+
+        void IncreaseIndex()
+        {
+            waypointIndex++;
+            if (waypointIndex >= waypoints.Length)
+            {
+                waypointIndex = 0;
+            }
+            transform.LookAt(waypoints[waypointIndex].position);
+        }
     }
 }
+
