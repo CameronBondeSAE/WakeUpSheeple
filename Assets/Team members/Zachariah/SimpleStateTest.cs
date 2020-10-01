510f1e0f97e004394111c2a67990214576da9b8e
Zachariah <1019559@student.sae.edu.au>
2020-10-01 14:09:21 +1000

    Creation of SimpleStateScript

--- Assets/Team members/Zachariah/SimpleStateTest.cs
+++ Assets/Team members/Zachariah/SimpleStateTest.cs
@@ -1,18 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class SimpleStateTest : MonoBehaviour
 {
+    
+    public DelegateStateManager DelegateStateManager = new DelegateStateManager();
+    
+    public DelegateState rest = new DelegateState();
+    public DelegateState active = new DelegateState();
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+        //this doesn't run the function. It just remembers it for later
+        rest.Enter = OnRestEnter;
+        rest.Update = OnRestUpdate;
+        rest.Exit = OnRestExit;
+
+        active.Enter = OnActiveEnter;
+        active.Update = OnActiveUpdate;
+        active.Exit = OnActiveExit;
+
+        DelegateStateManager.ChangeState(rest);
+    }
+
+    private void OnRestEnter()
+    {
+       Debug.Log("RestEnter");
+       GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    private void OnRestUpdate()
+    {
+        Debug.Log("RestUpdate");
+        if (Input.GetKey(KeyCode.Space))
+        {
+            DelegateStateManager.ChangeState(active);
+        }
+    }
+
+    private void OnRestExit()
+    {
+        Debug.Log("RestExit");
+    }
+
+    private void OnActiveEnter()
+    {
+       Debug.Log("ActiveEnter");
+       GetComponent<Renderer>().material.color = Color.magenta;
+       GetComponent<Rigidbody>().AddForce(1,1,0,ForceMode.VelocityChange);
+    }
+
+    private void OnActiveUpdate()
+    {
+        Debug.Log("ActiveUpdate");
+        if (Input.GetKey(KeyCode.KeypadEnter))
+        {
+            DelegateStateManager.ChangeState(rest);
+        }
+    }
+
+    private void OnActiveExit()
+    {
+        Debug.Log("ActiveExit");
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DelegateStateManager.UpdateCurrentState();
     }
 }
