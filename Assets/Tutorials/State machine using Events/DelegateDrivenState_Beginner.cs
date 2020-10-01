fb591900ad02967dd48098c6913b110d99b567a2
Cameron Bonde <c.bonde@sae.edu>
2020-10-01 11:20:06 +1000

    State machine examples

---
+++ Assets/Tutorials/State machine using Events/DelegateDrivenState_Beginner.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class DelegateDrivenState_Beginner : MonoBehaviour
+{
+	// You COULD make the statemanager a component, but I've just made it a pure c# class, so I have to 'new' it myself (unlike components/monobehaviours)
+	public DelegateStateManager DelegateStateManager = new DelegateStateManager();
+
+	public DelegateState idle = new DelegateState();
+	public DelegateState run  = new DelegateState();
+
+	void Start()
+	{
+		Debug.Log("Press space to change to run state. HACK: It's just to test");
+		
+		// I'm ASSIGNING the variables to specific functions. Remember, variable just POINT to things... ANY thing! ints, strings, gameobjects, components... and... FUNCTIONS!
+		idle.Enter   = OnIdleEnter;
+		idle.Update = OnIdleUpdate;
+		idle.Exit    = OnIdleExit;
+
+		run.Enter   = OnRunEnter;
+		run.Update = OnRunUpdate;
+
+		DelegateStateManager.ChangeState(idle);
+	}
+
+	void Update()
+	{
+		// Whatever the current state is, run it's update code
+		DelegateStateManager.UpdateCurrentState();
+	}
+
+	
+	
+	
+	private void OnRunEnter()
+	{
+		Debug.Log("OnRunEnter");
+	}
+
+	private void OnRunUpdate()
+	{
+		Debug.Log("OnRunExecute");
+	}
+
+	private void OnIdleEnter()
+	{
+		Debug.Log("OnIdleEnter");
+	}
+
+	private void OnIdleUpdate()
+	{
+		Debug.Log("OnIdleExecute");
+	
+		// Just me forcing the state to change for testing
+		// if (InputSystem.GetDevice<Keyboard>().spaceKey.wasPressedThisFrame)
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			DelegateStateManager.ChangeState(run);
+		}
+	}
+
+	private void OnIdleExit()
+	{
+		Debug.Log("OnIdleExit");
+	}
+}
---
+++ Assets/Tutorials/State machine using Events/EventDrivenState_Intermediate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+// using UnityEngine.InputSystem;
+
+public class EventDrivenState_Intermediate : MonoBehaviour
+{
+	public DelegateState idle = new DelegateState();
+	public DelegateState run  = new DelegateState();
+
+	public DelegateStateManager DelegateStateManager = new DelegateStateManager();
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		// Here I'm using "Lambdas" so I don't have to define whole functions for everything. You can mix and match.
+		idle.Enter   = () => Debug.Log("IDLE BEGIN");
+		idle.Update = () => Debug.Log("IDLE GO!");
+		idle.Exit    = () => Debug.Log("IDLE END");
+
+		run.Enter   = () => Debug.Log("run begin");
+		run.Update = () => Debug.Log("run execute");
+
+		DelegateStateManager.ChangeState(idle);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		DelegateStateManager.UpdateCurrentState();
+
+		// Just me forcing the state to change for testing
+		// if (InputSystem.GetDevice<Keyboard>().spaceKey.wasPressedThisFrame)
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			DelegateStateManager.ChangeState(run);
+		}
+	}
+}
