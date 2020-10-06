db967fa3b0ff0adc4fa1de613cac2a4ff84c02c8
ayounes2001 <1016744@student.sae.edu.au>
2020-10-06 17:09:48 +1100

    Clay Shader Update and Vertex Displacement

---
+++ Assets/Team members/Anthony Y/Scripts/ShaderRotaty.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderRotaty : MonoBehaviour
+{
+    public Material clay;
+    private static readonly int RotationAmount = Shader.PropertyToID("RotationAmount");
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        clay.SetInt(RotationAmount,Random.Range(0,360));
+    }
+}
