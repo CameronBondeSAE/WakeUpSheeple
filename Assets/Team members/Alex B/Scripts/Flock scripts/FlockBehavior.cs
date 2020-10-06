b350e19ae293af55c164e5859ae30c83e8b53678
AJ <1018212@student.sae.edu.au>
2020-10-06 15:40:11 +1100

    Started Flock

---
+++ Assets/Team members/Alex B/Scripts/Flock scripts/FlockBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AJ;
+using UnityEngine;
+
+namespace AJ
+{
+    public abstract class FlockBehavior : ScriptableObject
+    {
+        public abstract Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock);
+    }
+}
+
