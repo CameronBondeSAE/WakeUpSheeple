f7d56a82136cf16d6a806422e3afa1e7fb883712
Cameron Bonde <c.bonde@sae.edu>
2020-10-06 14:49:48 +1100

    Events tutorial. Pause manager demo

---
+++ Assets/Team members/Cam/PauseManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+	// Full event definition with delegate
+	public delegate void       PauseDelegate();
+	public event PauseDelegate PauseEvent;
+
+	// Shortcut lazy event definition
+	public event Action AnotherPauseEvent;
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			// YOU NEED to check for null. Because if nothing is 'listening' it will be calling NOTHING which will null
+
+			// Using .Invoke. I personally like this, because it shows that it's not a normal function
+			
+			// Full null check
+			if (AnotherPauseEvent != null)
+			{
+				AnotherPauseEvent.Invoke();
+			}
+			
+			// Checking for null shortcut using ?. Same as the if above
+			PauseEvent?.Invoke();
+		}
+	}
+}
