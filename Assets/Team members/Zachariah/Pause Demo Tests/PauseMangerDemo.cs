9604dd351e0edb6250b5491be081688bb0a05aed
Zachariah <1019559@student.sae.edu.au>
2020-10-08 14:15:49 +1100

    PauseManager and Virus Parent code creation

---
+++ Assets/Team members/Zachariah/Pause Demo Tests/PauseMangerDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMangerDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public event Action PauseTest;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PauseTest?.Invoke();
+        }
+
+    }
+}
