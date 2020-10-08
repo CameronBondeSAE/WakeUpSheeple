838b9c2fe88140d23ca6d262055e71a63c53fa4a
Riley McGowan <1017093@student.sae.edu.au>
2020-10-08 15:46:26 +1100

    Mole Adjustments
    Change to mole including trigger

---
+++ Assets/Team members/Riley/TriggerMoleR.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMoleR : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("platformR"))
+        {
+            Debug.Log("Collision Detected");
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.gameObject.CompareTag("platformR"))
+        {
+            Debug.Log("Collision Still Detected");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag("platformR"))
+        {
+            Debug.Log("Collision Removed");
+        }
+    }
+}
