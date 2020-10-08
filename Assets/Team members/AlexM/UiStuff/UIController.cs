be22eeeb5da90d3a8cfe1bcd5b01d730f532b300
Slexx <slex-@hotmail.com>
2020-10-08 15:58:14 +1100

    Saving Work before i lose it again.

---
+++ Assets/Team members/AlexM/UiStuff/UIController.cs
@@ -0,0 +1,27 @@
+﻿﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+	public GameObject Sheep;
+	public TextMeshProUGUI  debug_Velocity;
+	
+	private void OnEnable()
+	{
+		Sheep.GetComponent<Movement_ForwardAM>().velEvent += OnvelEvent;
+	}
+
+	private void OnvelEvent(Vector3 obj)
+	{
+		debug_Velocity.SetText(obj.ToString());
+	}
+
+	private void OnDisable()
+	{
+		Sheep.GetComponent<Movement_ForwardAM>().velEvent -= OnvelEvent;
+	}
+}
