98603dd0d9f40bdc1e226e79c56b00219dcb4526
Riley McGowan <1017093@student.sae.edu.au>
2020-10-15 10:34:55 +1100

    Waypointing Upload
    Waypoints work with individual script but statemachine influence causes it to break for some reason.

---
+++ Assets/Team members/Riley/waypointsScriptR.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class waypointsScriptR : MonoBehaviour
+{
+    public List<Transform> waypointsList = new List<Transform>();
+    private Transform currentWaypoint;
+    private int currentWaypointIndex;
+    private float safeDistance = 0.1f;
+    private int previousWaypointIndex;
+    private float movementSpeed = 0.05f;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentWaypointIndex = 0;
+        currentWaypoint = waypointsList[currentWaypointIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, currentWaypoint.position) > safeDistance)
+        {
+            transform.LookAt(currentWaypoint);
+            rb.AddRelativeForce(Vector3.forward * movementSpeed, ForceMode.Force);
+        }
+    }
+}
