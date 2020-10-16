a3a732ea4bf45eeb9f65894e794e0e0178dc5484
Slexx <slex-@hotmail.com>
2020-10-17 01:23:02 +1100

    made great progress on the first attempt of the achievement manager... not sure if its the right idea yet...

---
+++ Assets/Team members/AlexM/Scripts/Editor/AchievementEditor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace AlexM
+{
+    [CustomEditor(typeof(AchievementManager))]
+    public class AchievementEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            EditorGUILayout.LabelField(" ");
+            EditorGUILayout.LabelField("Debugging", EditorStyles.boldLabel);
+
+            GUILayout.BeginHorizontal();
+            
+            if (GUILayout.Button("ToggleTest", GUILayout.Width(80)))
+            {
+                ((AchievementManager) target).ToggleGameObjects();
+            }
+
+            if (GUILayout.Button("Quick Setup", GUILayout.Width(80)))
+            {
+                ((AchievementManager) target).QuickSetup();
+            }
+
+            GUILayout.EndHorizontal();
+        }
+    }
+
+}
