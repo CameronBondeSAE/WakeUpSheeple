152a4c3ade926bab4413a7054a6f12ef595761f2
Luke <1019817@student.sae.edu.au>
2020-10-19 15:44:24 +1100

    implemented EnergiseBehaviour script

---
+++ Assets/Team members/Luke Baker/5G tower test/EnergiseBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class EnergiseBehaviour : MonoBehaviour
+{
+    //variables
+    public int timeEffected = 3;
+
+    IEnumerator EnergiseForTime()
+    {
+        //apply the energise buff here
+        //gameObject.GetComponent<Movement>.speed * 3 ??
+
+        yield return new WaitForSeconds(timeEffected);
+        
+        //removes the script from the object
+        Destroy(this);
+    }
+
+    private void Update()
+    {
+        StartCoroutine(EnergiseForTime());
+    }
+}
