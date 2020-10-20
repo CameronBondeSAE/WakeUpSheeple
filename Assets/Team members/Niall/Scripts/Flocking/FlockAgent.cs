e5cbb20b09df66d2212495c09bc834c66d5ece45
Niall <1013734@student.sae.edu.au>
2020-10-20 14:29:34 +1100

    testing prefab removal
    g

--- Assets/Team members/Niall/Scripts/Flocking/FlockAgent.cs
+++ Assets/Team members/Niall/Scripts/Flocking/FlockAgent.cs
@@ -26,8 +26,10 @@ namespace Niall
         public void Move(Vector3 velocity)
         {
             transform.forward = velocity;
+            
             rb.AddForce(velocity * Time.deltaTime);
            // transform.position += velocity * Time.deltaTime;
+
         }
     }
 }
