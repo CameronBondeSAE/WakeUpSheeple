8177695faa8755a32d6718555efc91011b814d3e
Cameron Bonde <c.bonde@sae.edu>
2020-10-22 16:04:51 +1100

    CharacterBase base class for all characters. Inherit from this class from your main script (or if it doesn't have a main script, just attach it directly)

---
+++ Assets/Team members/Cam/KillAllCharacters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillAllCharacters : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		CharacterBase characterBase = other.GetComponent<CharacterBase>();
+		characterBase.isAwesome = false;
+		
+		if (characterBase)
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
