5e446488c8acaabe0b40e3453beda4d2098b9826
Cameron Bonde <c.bonde@sae.edu>
2020-10-29 17:08:46 +1100

    Updated prefab test scene

---
+++ Assets/Misc/RandomMover.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMover : MonoBehaviour
+{
+	public bool  randomWibble = true;
+	Vector3      startPos;
+	public float amount = 3f;
+
+	// Start is called before the first frame update
+    void Start()
+	{
+		startPos = transform.position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (randomWibble)
+		{
+			transform.position = startPos + new Vector3(-amount/2f,0,-amount/2f) + new Vector3(Mathf.PerlinNoise(0, Time.time), 0, Mathf.PerlinNoise(0, 10000+Time.time)) * amount;
+		}
+	}
+}
