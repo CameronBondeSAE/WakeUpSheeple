db4234ac3ae1bfd65ae247c751a8fe12be6f2479
Zachariah <1019559@student.sae.edu.au>
2020-10-29 14:28:13 +1100

    Creation of Lava and LavaSacrifice Prefab

---
+++ Assets/Team members/Zachariah/LavaScript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<Health>().Damage(1000);
+    }
+}
