d84f7aa8c17f29f259466fb7f081dd9a7b3a9007
Cameron Bonde <c.bonde@sae.edu>
2020-10-29 11:09:09 +1100

    Example for interacting with the Health component

---
+++ Assets/Tutorials/Health and dying/DyingDude.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DyingDude : MonoBehaviour
+{
+	public Health health;
+
+
+	void OnEnable()
+	{
+		health.DeathEvent += HealthOnDeathEvent;
+	}
+
+	void OnDisable()
+	{
+		health.DeathEvent -= HealthOnDeathEvent;
+	}
+
+	void HealthOnDeathEvent(Health obj)
+	{
+		// My death code goes here
+		// - This is a demo only, you do whatever YOUR object does when it dies
+		Destroy(gameObject);
+	}
+}
