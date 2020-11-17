ab92e9e803fd96c3a443454a3579a7256cb0b7a6
Damien Harwood <1019243@student.sae.edu.au>
2020-11-17 15:00:13 +1100

    Started TV Prefab

---
+++ Assets/Team members/Damien/Scripts/PropagandaTV.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropagandaTV : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        DetectSheep();
+    }
+    
+    public void DetectSheep()
+    {
+        
+    }
+
+
+    public IEnumerator PlayVideo()
+    {
+        //PlayVideo on TV
+        yield return new WaitForSeconds(10f);
+    }
+}
