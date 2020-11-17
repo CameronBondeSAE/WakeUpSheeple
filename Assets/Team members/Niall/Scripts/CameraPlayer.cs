b9514b3dec9b665a7ad851d3297164a0edeb5a09
Niall <1013734@student.sae.edu.au>
2020-11-17 17:44:48 +1100

    weathermananger started, player camera v1 working

---
+++ Assets/Team members/Niall/Scripts/CameraPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using AnthonyY;
+using DG.Tweening;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraPlayer : MonoBehaviour
+{
+    public Transform target;
+    public Camera cam;
+    public float smoothSpeed;
+    public Vector3 offset;
+    
+    private Vector3 velocity;
+
+    private void Awake()
+    {
+        FindObjectOfType<GameManager>().playersSpawnedEvent += OnPlayersSpawnedEvent;
+        cam = GetComponent<Camera>();
+    }
+
+    private void OnPlayersSpawnedEvent(PlayerBehaviour player)
+    {
+       // if (player.isLocalPlayer)
+      //  {
+     //      target = player.transform;
+     //   }
+        
+       target = player.transform;
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+        
+        
+        if (target != null)
+        {
+            
+            transform.position = Vector3.SmoothDamp(transform.position, target.transform.position + offset,
+                ref velocity, smoothSpeed);
+            
+        }
+    }
+}
