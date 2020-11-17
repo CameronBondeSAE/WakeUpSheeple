dc0153d2f44a53f97cd2b905ee3835ee338b3556
Cameron Bonde <c.bonde@sae.edu>
2020-11-17 15:07:20 +1100

    Maths tests

---
+++ Assets/Tutorials/Maths experiments/MathsFUN.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MathsFUN : MonoBehaviour
+{
+	public LineRenderer LineRenderer;
+
+	public float scale = 5f;
+
+	void Update()
+	{
+		float y = 0;
+
+		LineRenderer.positionCount = 10;
+
+		float acceleration = 0;
+
+		for (int x = 0; x < 11; x++)
+		{
+			acceleration = acceleration + 1f;
+
+			y = y + acceleration * scale;
+
+			// Visualise
+			LineRenderer.SetPosition(x, new Vector3(x, y, 0));
+		}
+	}
+}
