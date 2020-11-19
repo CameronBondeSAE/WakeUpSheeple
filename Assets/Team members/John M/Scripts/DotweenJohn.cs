4ea9a04150dda1b0901b1728f91f2eddb85df077
John <1019178@student.sae.edu.au>
2020-11-19 11:27:24 +1100

    DoTween test scene and Nuke Test added
    explosion effect is present on nuke
    future functionality needed to trigger on impact with ground
    AOE  effect to be added

---
+++ Assets/Team members/John M/Scripts/DotweenJohn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class DotweenJohn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float xScale = 1f;
+    public float yScale = 3f;
+    public float zScale = 15f;
+    public float duration = 1.5f;
+
+    void Start()
+    {
+        Sequence summon = DOTween.Sequence();
+        summon.Append(transform.DOScale(new Vector3(xScale, yScale, zScale), duration));
+        summon.Append(transform.DOLookAt(new Vector3(0, 25), duration));
+    }
+}
---
+++ Assets/Team members/John M/Scripts/NukeBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NukeBehaviour : MonoBehaviour
+{
+    // FUNCTIONS REQUIRED
+    
+    // if nuke collides with the ground, trigger explosion
+    // explode the nuke
+    // leave behind radiation zone after explosion
+    // radiation zone disappears over time
+    
+    void Update()
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                Detonate();
+            }
+        }
+
+        void Detonate()
+        {
+            
+        }
+
+
+
+    }
+
+    
+}
