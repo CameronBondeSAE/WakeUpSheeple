1321240ce083741035dd3d3c86164ad64154dcfe
Riley McGowan <1017093@student.sae.edu.au>
2020-11-24 14:57:15 +1100

    Quick rain fix to demo scene
    Rain wasn't consistent between both objects and using two sliders.

--- Assets/Team members/Riley/Scripts/RainHandler.cs
+++ Assets/Team members/Riley/Scripts/RainHandler.cs
@@ -6,20 +6,33 @@ public class RainHandler : MonoBehaviour
 {
     public float rainSlider = 0.0F;
     private ParticleSystem ps;
-    
+    private ParticleSystem cps;
+    public Transform cameraTransform;
+    public GameObject childObject;
+
     void Start()
     {
         ps = GetComponent<ParticleSystem>();
+        cps = childObject.GetComponent<ParticleSystem>();
     }
     void Update()
     {
+        // ----- Vars to edit our downfall rate ----- //
         var emission = ps.emission;
         var main = ps.main;
+        var mainCO = cps.main;
+        var emissionCO = cps.emission;
         main.maxParticles = Mathf.RoundToInt(rainSlider);
         emission.rateOverTime = Mathf.RoundToInt(rainSlider);
+        mainCO.maxParticles = Mathf.RoundToInt(rainSlider);
+        emissionCO.rateOverTime = Mathf.RoundToInt(rainSlider);
+        // ----- Moves object to camera ----- //
+        Vector3 cameraPos = new Vector3(cameraTransform.position.x, cameraTransform.position.y + 5, cameraTransform.position.z);
+        transform.position = cameraPos;
     }
     void OnGUI()
     {
+        // ----- Quick GUI Slider ----- //
         rainSlider = GUI.HorizontalSlider(new Rect(25, 25, 100, 30), rainSlider, 0.0F, 1000.0F);
     }
 }
