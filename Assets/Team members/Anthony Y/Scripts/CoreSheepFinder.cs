9eaf44168cc2f4529d8b11b166f4d57546ecff65
ayounes2001 <1016744@student.sae.edu.au>
2020-11-24 13:53:48 +1100

    Start of Sheep Finder

---
+++ Assets/Team members/Anthony Y/Scripts/CoreSheepFinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AlexM;
+using Mirror;
+using UnityEngine;
+using FlockBehavior = AJ.FlockBehavior;
+
+public class CoreSheepFinder : NetworkBehaviour
+{
+    public NeigboursDetector neighboursDetect;
+// [SyncVar]
+//     public Sheep centerofFlockSheep;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(MassFlockAmount());
+    }
+
+    IEnumerator MassFlockAmount()
+    {
+        foreach (Sheep sheep in neighboursDetect.GetNearbyObjects())
+        {
+           sheep.GetComponent<NeigboursDetector>().GetNearbyObjects();
+        }
+        yield return new WaitForSeconds(2f);
+    }
+}
+
+
+//foreach through all sheep (use a coroutine to only do it every few seconds for efficiency)
+// every sheep has a 'Neighbours' script
+// Keep track of the highest neighbours count
+// have a single 'centerOfFlockSheep'
+// NetworkIdentity variable (because netid is automatically synced to clients)
+
