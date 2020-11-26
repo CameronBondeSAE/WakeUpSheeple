cd0ccc96788f850c2aceee1fa7680a97b8269c9c
Riley McGowan <1017093@student.sae.edu.au>
2020-11-26 12:56:02 +1100

    Road addforce to waypoint change
    The road prefab has been mostly completed and changed to the waypoint system rather than apply force

--- Assets/Team members/Riley/Scripts/CarHandler.cs
+++ Assets/Team members/Riley/Scripts/CarHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using NodeCanvas.Tasks.Conditions;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,21 +9,20 @@ public class CarHandler : MonoBehaviour
 {
     public GameObject carRespawnObject;
     private Vector3 carRespawnPoint;
-    // Start is called before the first frame update
-    void Start()
-    {
-        carRespawnPoint = new Vector3(carRespawnObject.transform.position.x, carRespawnObject.transform.position.y + 1.5f, carRespawnObject.transform.position.z);
-    }
-    // Update is called once per frame
+    public GameObject destinationWaypoint;
+    public float speed = 15f;
+    private float safeDistance = 5f;
     void Update()
     {
-        
-    }
-    
-    
+        carRespawnPoint = new Vector3(carRespawnObject.transform.position.x, carRespawnObject.transform.position.y, carRespawnObject.transform.position.z); //Takes our respawn point
+        if (destinationWaypoint != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, destinationWaypoint.transform.position, speed * Time.deltaTime); //move towards waypoint
+        }
 
-    private void OnTriggerExit(Collider other)
-    {
-        other.transform.position = carRespawnPoint;
+        if (Vector3.Distance(transform.position, destinationWaypoint.transform.position) < safeDistance)
+        {
+            transform.position = carRespawnPoint;
+        }
     }
 }
--- Assets/Team members/Riley/Scripts/RoadHandler.cs
+++ Assets/Team members/Riley/Scripts/RoadHandler.cs
@@ -6,22 +6,14 @@ using UnityEngine;
 public class RoadHandler : MonoBehaviour
 {
     public GameObject CarObjectToSpawn;
+    public GameObject CarEndWaypoint;
     private Vector3 carVector;
-    private Rigidbody rb;
-    private float carSpeed = 50f;
     GameObject instantiate;
-    // Start is called before the first frame update
     void Start()
     {
-        carVector = new Vector3(transform.position.x, transform.position.y + 1.5f, transform.position.z);
-        
-        instantiate = Instantiate<GameObject>(CarObjectToSpawn, carVector, transform.rotation);
-        rb = instantiate.GetComponent<Rigidbody>();
-        //FindObjectOfType<CarHandlerR>().DeleteEvent += deleteEvent;
-        rb.AddForce(-transform.right * carSpeed);
-    }
-    // Update is called once per frame
-    void Update()
-    {
+        carVector = new Vector3(transform.position.x, transform.position.y, transform.position.z); //Place we spawn
+        instantiate = Instantiate<GameObject>(CarObjectToSpawn, carVector, transform.rotation); //Object spawned (Keep for use)
+        instantiate.GetComponent<CarHandler>().carRespawnObject = gameObject;
+        instantiate.GetComponent<CarHandler>().destinationWaypoint = CarEndWaypoint;
     }
 }
