7d96a01aa3ecb900eda77da2ed7184b566c8d06c
ayounes2001 <1016744@student.sae.edu.au>
2020-12-01 16:52:01 +1100

    Revert "Changing between a dog and a wolf"
    This reverts commit 0542a45935a8c4e148507925c58e2dbf9548c7f3.

--- Assets/Team members/Anthony Y/Scripts/PlayerBehaviour.cs
+++ Assets/Team members/Anthony Y/Scripts/PlayerBehaviour.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Security;
 using Mirror;
 using Mirror.Examples.Chat;
 using Student_workspace.Dylan.Scripts.NetworkLobby;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 namespace AnthonyY
 {
-	
 	public class PlayerBehaviour : NetworkBehaviour, IOwnable
 	{
 		[SyncVar]
@@ -21,25 +17,15 @@ namespace AnthonyY
 		public Health health;
 
 		public PlayerControls controls;
-		public AudioSource dogBark;
-		public AudioSource wolfHowl;
+		public AudioSource    dogBark;
+		public AudioSource    wolfHowl;
 
 		[SerializeField]
 		private bool amIWolf = false;
 
-		private Vector3 movement;
-		private Rigidbody rb;
-		[SerializeField]
-		private NetworkIdentity _owner;
-
 		private void Awake()
 		{
 			controls = new PlayerControls();
-		}
-
-		private void Start()
-		{
-			 rb = GetComponent<Rigidbody>();
 		}
 
 
@@ -48,10 +34,10 @@ namespace AnthonyY
 			controls.Dog.Enable();
 			controls.Wolf.Enable();
 			controls.Movement.Enable();
-			health.DeathEvent += Death;
-			controls.Dog.Bark.performed += _ => RpcBark();
-			controls.Wolf.Howl.performed += _ => RpcHowl();
-			// GetComponent<PauseManager>().PauseEvent += OnPauseEvent;
+			health.DeathEvent                       += Death;
+			controls.Dog.Bark.performed             += _ => RpcBark();
+			controls.Wolf.Howl.performed            += _ => RpcHowl();
+			GetComponent<PauseManager>().PauseEvent += OnPauseEvent;
 		}
 
 		private void OnPauseEvent()
@@ -67,79 +53,57 @@ namespace AnthonyY
 			health.DeathEvent -= Death;
 		}
 
-		public NetworkIdentity Owner
-		{
-			get => _owner;
-			set => _owner = value;
-		}
+		public NetworkIdentity Owner { get; set; }
 
 		void Update()
 		{
-			Debug.Log(Owner.isLocalPlayer);
-			    if (Owner.isLocalPlayer)
-				{
-					Vector3 movementInput = controls.Movement.Movement.ReadValue<Vector2>();
-					CmdMove(movementInput);
-				}
-				
-
-				// transform.Translate(movement * (movementSpeed * Time.deltaTime), Space.World);
-				rb.velocity = movement * (movementSpeed * Time.deltaTime);
-
-				// Are we moving AT ALL?
-				if (rb.velocity.magnitude > 0.5f)
-				{
-					transform.rotation = Quaternion.LookRotation(movement);
-				}
-
-
-				Vector3 playerVelocity = rb.velocity;
-				playerVelocity.y = 0;
-				transform.forward = new Vector3(playerVelocity.x, playerVelocity.y, playerVelocity.z);
+			// Debug.Log(Owner);
+			// if (isClient)
+			// {
+			//     if (Owner != null)
+			//     {
+			//         if (Owner.isLocalPlayer)
+			//         {
+			//             Vector3 movementInput = controls.GamePlayer.Movement.ReadValue<Vector2>();
+			//             CmdMove(movementInput);
+			//         }
+			//     }
+			// }
+			Vector3 movementInput = controls.Movement.Movement.ReadValue<Vector2>();
+			CmdMove(movementInput);
 		}
 
 		[ClientRpc]
-		private void RPCCheckNewInput(Vector2 movementInput)
+		private void RpcMove(Vector2 movementInput)
 		{
-		 movement	= new Vector3
+			Vector3 movement = new Vector3
+							   {
+								   x = movementInput.x,
+								   z = movementInput.y
+							   }.normalized;
+
+
+			Rigidbody rb = GetComponent<Rigidbody>();
+
+			// transform.Translate(movement * (movementSpeed * Time.deltaTime), Space.World);
+			rb.velocity = movement * (movementSpeed * Time.deltaTime);
+
+			// Are we moving AT ALL?
+			if (rb.velocity.magnitude > 0.5f)
 			{
-				x = movementInput.x,
-				z = movementInput.y
-			}.normalized;
-		 
+				transform.rotation = Quaternion.LookRotation(movement);
+			}
+
+
+			Vector3 playerVelocity = rb.velocity;
+			playerVelocity.y  = 0;
+			transform.forward = new Vector3(playerVelocity.x, playerVelocity.y, playerVelocity.z);
 		}
 
 		public void RpcBark()
 		{
 			dogBark.Play();
 			Debug.Log("Dog Audio Played");
-			
-		}
-
-		public void TurnIntoDog()
-		{
-			amIWolf = false;
-			if (amIWolf == false)
-			{
-				Debug.Log("I am now a dog");
-			
-				controls.Dog.Enable();
-				controls.Wolf.Disable();
-			}
-			
-		}
-
-		public void TurnIntoWolf()
-		{
-			amIWolf = true;
-			if (amIWolf)
-			{
-				Debug.Log("I am now a wolf");
-				controls.Wolf.Enable();
-				controls.Dog.Enable();
-				
-			}
-			
 		}
 
 
@@ -160,8 +124,7 @@ namespace AnthonyY
 		[Command]
 		private void CmdMove(Vector2 movementInput)
 		{
-			RPCCheckNewInput(movementInput);
+			RpcMove(movementInput);
 		}
-		
 	}
 }
