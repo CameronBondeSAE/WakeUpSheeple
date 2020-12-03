8deb2f0a9f41b7ca835987652b8a25e4dd035b4a
Niall <1013734@student.sae.edu.au>
2020-12-03 13:02:42 +1100

    tutorial camera start (bounds) behavior tree looksy
    ill look into cinemachine path camera

---
+++ Assets/Team members/Niall/Scripts/Cameras/CameraPlayer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using AnthonyY;
+using Cinemachine;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Niall
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraPlayer : MonoBehaviour
+    {
+        public Transform OwnPlayer;
+        public float smoothSpeed;
+        public Vector3 offset;
+
+        private Vector3 velocity;
+
+        private void Awake()
+        {
+            FindObjectOfType<GameManager>().playersSpawnedEvent += OnPlayersSpawnedEvent;
+        }
+
+        private void OnPlayersSpawnedEvent(PlayerBehaviour player)
+        {
+             if (player.isLocalPlayer)
+              {
+                  OwnPlayer = player.transform;
+               }
+            else
+            OwnPlayer = player.transform;
+        }
+
+
+        private void LateUpdate()
+        {
+            Move();
+        }
+
+        void Move()
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, OwnPlayer.transform.position + offset,
+                ref velocity, smoothSpeed);
+        }
+    }
+}
