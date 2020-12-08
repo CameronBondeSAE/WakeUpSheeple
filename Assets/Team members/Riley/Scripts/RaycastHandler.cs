26a39f2204e2e2fb8a941b7da27e83cc7c396796
Riley McGowan <1017093@student.sae.edu.au>
2020-12-08 15:37:19 +1100

    Prefab Update for Mole Man including Layer fix
    Proper working fix for the mole man prefab so almost no setup is required for it to function. The layer is now fixed so it ignores other objects when moving.

--- Assets/Team members/Riley/Scripts/RaycastHandler.cs
+++ Assets/Team members/Riley/Scripts/RaycastHandler.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class RaycastHandler : MonoBehaviour
 {
     public float distanceToPlatformInfo;
+    public float redRayLine;
+    public LayerMask RaycastHitLayer;
     void Update()
     {
         Ray ray = new Ray(transform.position, Vector3.down);
         RaycastHit raycastToPlatform;
-        if (Physics.Raycast(ray, out raycastToPlatform))
+        if (Physics.Raycast(ray, out raycastToPlatform, 500, RaycastHitLayer))
         {
             Debug.DrawLine(ray.origin, raycastToPlatform.point, Color.green);
         }
         else
         {
-            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.red);
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * redRayLine, Color.red);
         }
         distanceToPlatformInfo = raycastToPlatform.distance;
     }
