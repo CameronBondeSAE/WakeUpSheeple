0013af4904598fb5cbe753f6c3f7d7dca2edfaf7
tim pearson <1018360@student.sae.edu.au>
2020-12-08 14:58:10 +1100

    added movement to black helicopter and added buttons for future use

---
+++ Assets/Team members/TimPearson/Editor/BlackHelicopterEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Tim;
+using UnityEditor;
+
+
+[CustomEditor(typeof(BlackHelicopter))]
+public class BlackHelicopterEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Strafe Off"))
+        {
+            (target as BlackHelicopter)?.StrafeOff();
+        }
+
+        if (GUILayout.Button("Strafe On"))
+        {
+            (target as BlackHelicopter)?.StrafeOn();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
