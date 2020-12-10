70aa7743fc279de5ee231fef2be27af1a9bd541c
Damien Harwood <1019243@student.sae.edu.au>
2020-12-10 15:11:15 +1100

    Bills gate prefab change

--- Assets/Team members/Damien/Scripts/PropagandaTV.cs
+++ Assets/Team members/Damien/Scripts/PropagandaTV.cs
@@ -9,17 +9,24 @@ namespace Damien
 {
     public class PropagandaTV : MonoBehaviour
     {
-        public float tvInnerRadius = 10f;
-        public float tvOuterRadius = 15f;
+        public float tvInnerRadius = 5f;
+        public float tvOuterRadius = 10f;
         public bool isOn = true;
 
-        public float tvAngle;
+        public float tvAngle = 45f;
 
         public GameObject sheep;
+
+        public Vector3 targetDirection;
+        public Vector3 myPos;
+        public PropagandaTV target;
+
+        public Rigidbody rb;
+
         // Start is called before the first frame update
         void Start()
         {
-            StartCoroutine(PlayVideo());
+
         }
 
         // Update is called once per frame
@@ -65,37 +72,37 @@ namespace Damien
 
         public IEnumerator HypnotiseSheep()
         {
-            
+            sheep.GetComponent<Movement_ForwardAM>().enabled = false;
             yield return new WaitForSeconds(5f);
         }
 
-        public IEnumerator LureSheep()
-        {
-            //TODO:Maybe move sheep towards inner circle?
-            yield return new WaitForSeconds(5f);
-        }
+            public IEnumerator LureSheep()
+            {
+                //TODO:Maybe move sheep towards inner circle?
+                yield return new WaitForSeconds(5f);
+            }
 
-        public IEnumerator PlayVideo()
-        {
-            //TODO:PlayVideo on TV
+            public IEnumerator PlayVideo()
+            {
+                //TODO:PlayVideo on TV
 
-            yield return new WaitForSeconds(10f);
-        }
+                yield return new WaitForSeconds(10f);
+            }
 
-        private void OnDrawGizmosSelected()
-        {
-            Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(transform.position, tvInnerRadius);
+            private void OnDrawGizmosSelected()
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireSphere(transform.position, tvInnerRadius);
 
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawWireSphere(transform.position, tvOuterRadius);
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireSphere(transform.position, tvOuterRadius);
 
-            Vector3 fovLine1 = Quaternion.AngleAxis(tvAngle, transform.up) * transform.forward * tvInnerRadius;
-            Vector3 fovLine2 = Quaternion.AngleAxis(-tvAngle, transform.up) * transform.forward * tvInnerRadius;
+                Vector3 fovLine1 = Quaternion.AngleAxis(tvAngle, transform.up) * transform.forward * tvInnerRadius;
+                Vector3 fovLine2 = Quaternion.AngleAxis(-tvAngle, transform.up) * transform.forward * tvInnerRadius;
 
-            Gizmos.color = Color.green;
-            Gizmos.DrawRay(transform.position, fovLine1);
-            Gizmos.DrawRay(transform.position, fovLine2);
+                Gizmos.color = Color.green;
+                Gizmos.DrawRay(transform.position, fovLine1);
+                Gizmos.DrawRay(transform.position, fovLine2);
+            }
         }
     }
-}
