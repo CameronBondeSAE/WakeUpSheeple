071bf222f6b413c271edc1db963826bb347c74ab
Cam <csda>
2020-12-15 18:08:33 +1100

    Networked spawner works now.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -13,23 +13,22 @@ public class Spawner : NetworkBehaviour
 	public event Action<CharacterBase> SpawnedEvent;
 
 	// Start is called before the first frame update
-	void Start()
+	public override void OnStartServer()
 	{
-		if (isServer)
+		base.OnStartServer();
+		for (int i = 0; i < amount; i++)
 		{
-			for (int i = 0; i < amount; i++)
-			{
-				Vector2 insideUnitCircle       = Random.insideUnitCircle;
-				Vector3 randomInsideUnitCircle = new Vector3(insideUnitCircle.x, 0, insideUnitCircle.y);
+			Vector2 insideUnitCircle       = Random.insideUnitCircle;
+			Vector3 randomInsideUnitCircle = new Vector3(insideUnitCircle.x, 0, insideUnitCircle.y);
 
-				GameObject newGo = Instantiate(prefab, transform.position + (randomInsideUnitCircle * radius), Quaternion.Euler(0, Random.Range(0, 360), 0));
+			GameObject newGo = Instantiate(prefab, transform.position + (randomInsideUnitCircle * radius), Quaternion.Euler(0, Random.Range(0, 360), 0));
 
-				NetworkServer.Spawn(newGo);
-			
-				newGo.name = prefab.name + " " + i.ToString("##");
-				SpawnedEvent?.Invoke(newGo.GetComponent<CharacterBase>());
-			}
+
+			newGo.name = prefab.name + " " + i.ToString("##");
+
+			NetworkServer.Spawn(newGo);
+
+			SpawnedEvent?.Invoke(newGo.GetComponent<CharacterBase>());
 		}
-		
 	}
 }
