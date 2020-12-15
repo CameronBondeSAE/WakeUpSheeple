6e29f4beea3365c7338d52e0ef776fa678f66fb8
ayounes2001 <1016744@student.sae.edu.au>
2020-12-15 11:57:34 +1100

    New Level, terrains lowered the fps and updating my old level

---
+++ Assets/Team members/Anthony Y/Scripts/TextAtCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AnthonyY;
+using Student_workspace.Dylan.Scripts.NetworkLobby;
+using TMPro;
+using UnityEngine;
+
+public class TextAtCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshPro playerName;
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerName.text = GetComponent<PlayerBehaviour>().Owner.GetComponent<NetworkGamePlayer>().displayName;
+    }
+}
