ce8e828ebe8d1ee9e18d3e31da843f0ba46dd1eb
AJ <1018212@student.sae.edu.au>
2020-12-15 17:00:03 +1100

    continuing music manager and started weather manager and updated tween a bit

---
+++ Assets/Team members/Alex B/Scripts/AI scripts/TweenScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+namespace AJ
+{
+    public class TweenScript : MonoBehaviour
+    {
+        public Transform cubeTransform;
+        public Ease animEase;
+        public float animDuration;
+        // Start is called before the first frame update
+        void Start()
+        {
+            cubeTransform
+                .DOMoveY(5f, animDuration)
+                .SetEase(animEase)
+                .SetLoops(-1, LoopType.Incremental);
+        }
+    }
+}
+
