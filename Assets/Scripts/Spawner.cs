a9992d375498182f8f6cb779c6192a119d9f49c4
Cam <csda>
2020-12-15 17:47:07 +1100

    Networked spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Spawner : MonoBehaviour
+public class Spawner : NetworkBehaviour
 {
 	public GameObject                  prefab;
 	public float                       radius;
@@ -14,14 +15,21 @@ public class Spawner : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		for (int i = 0; i < amount; i++)
+		if (isServer)
 		{
-			Vector2 insideUnitCircle       = Random.insideUnitCircle;
-			Vector3 randomInsideUnitCircle = new Vector3(insideUnitCircle.x, 0, insideUnitCircle.y);
+			for (int i = 0; i < amount; i++)
+			{
+				Vector2 insideUnitCircle       = Random.insideUnitCircle;
+				Vector3 randomInsideUnitCircle = new Vector3(insideUnitCircle.x, 0, insideUnitCircle.y);
 
-			GameObject newGo = Instantiate(prefab, transform.position + (randomInsideUnitCircle * radius), Quaternion.Euler(0, Random.Range(0, 360), 0));
-			newGo.name = prefab.name + " " + i.ToString("##");
-			SpawnedEvent?.Invoke(newGo.GetComponent<CharacterBase>());
+				GameObject newGo = Instantiate(prefab, transform.position + (randomInsideUnitCircle * radius), Quaternion.Euler(0, Random.Range(0, 360), 0));
+
+				NetworkServer.Spawn(newGo);
+			
+				newGo.name = prefab.name + " " + i.ToString("##");
+				SpawnedEvent?.Invoke(newGo.GetComponent<CharacterBase>());
+			}
 		}
+		
 	}
 }
