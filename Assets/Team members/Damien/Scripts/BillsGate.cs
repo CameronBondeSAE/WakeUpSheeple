7c3b6b0c3e0856852249b22ea5ec71da5070cdfb
Cam <csda>
2020-12-15 17:08:43 +1100

    Changed to lerping value

--- Assets/Team members/Damien/Scripts/BillsGate.cs
+++ Assets/Team members/Damien/Scripts/BillsGate.cs
@@ -1,9 +1,7 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using AlexM;
 using AnthonyY;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Damien
 {
@@ -56,25 +54,31 @@ namespace Damien
 
             float shortestDistance = Mathf.Infinity;
             PlayerBehaviour nearestPlayer = null;
-            if (overlapSphere.Length > 0)
+            
+			
+			if (overlapSphere.Length > 0)
             {
-                //opening gate
-                if (partToRotate.transform.localRotation.eulerAngles.y < openAngle)
-                {
-                    //  Debug.Log("Rotation is less than 90");
-                    partToRotate.transform.Rotate(0f, rotateAngle * Time.deltaTime, 0f);
-                }
-            }
 
-            if (overlapSphere.Length == 0)
-            {
-                //closing gate
-                if (partToRotate.transform.eulerAngles.y > 0f)
-                {
-                    //   Debug.Log("Rotation is more than 0");
-                    partToRotate.transform.Rotate(0f, -rotateAngle * Time.deltaTime, 0f);
-                }
-            }
+                //opening gate
+				partToRotate.localRotation = Quaternion.Euler(0, Mathf.SmoothStep(partToRotate.transform.localRotation.eulerAngles.y, openAngle, Time.deltaTime * 10f),0);
+				Debug.Log(partToRotate.transform.localRotation.eulerAngles.y);
+
+				// if (partToRotate.transform.localRotation.eulerAngles.y < openAngle)
+				// {
+				//     //  Debug.Log("Rotation is less than 90");
+				//     partToRotate.transform.Rotate(0f, rotateAngle * Time.deltaTime, 0f);
+				// }
+			}
+
+            // if (overlapSphere.Length == 0)
+            // {
+            //     //closing gate
+            //     if (partToRotate.transform.eulerAngles.y > 0f)
+            //     {
+            //         //   Debug.Log("Rotation is more than 0");
+            //         partToRotate.transform.Rotate(0f, -rotateAngle * Time.deltaTime, 0f);
+            //     }
+            // }
         }
     }
 }
