1e9bba998095ee397a7196f626f5a3aea449ac5d
Riley McGowan <1017093@student.sae.edu.au>
2020-12-17 20:35:13 +1100

    Cloud and Rain now work with Weather Manager
    Clouds can spawn and lightning is rare spawn off of clouds.

---
+++ Assets/Team members/Zachariah/Editor/CloudEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Zach;
+
+[CustomEditor(typeof(Cloud))] //Set this as the script we are controlling
+public class CloudEditor : Editor //inherit from editor
+{
+    public override void OnInspectorGUI() //use the gui
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Spawn Lightning")) //This makes our button visible in the editor of unity
+        {
+            Debug.Log("Forced Lightning chance to 100%"); //Debug to prove it works
+            (target as Cloud)?.ForceLightning(); //Set the function to what we want the button to do
+        }
+    }
+}
