d02f252de5eacf1e5680b6375654239456050e97
Cameron Bonde <c.bonde@sae.edu>
2020-12-23 00:02:54 +1100

    Thread tutes

---
+++ Assets/Tutorials/Threading/JobTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs;
+using UnityEngine;
+
+public class JobTests : MonoBehaviour
+{
+	public JobHandle jobHandle1;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		for (int i = 0; i < 100; i++)
+		{
+			JobTest camJob = new JobTest();
+			jobHandle1 = camJob.Schedule();
+		}
+
+    }
+}
---
+++ Assets/Tutorials/Threading/ThreadTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+public class ThreadTest : MonoBehaviour
+{
+	public float total = 0;
+	object       aLock;
+
+    // Start is called before the first frame update
+    void Start()
+	{
+		for (int i = 0; i < 100; i++)
+		{
+			Thread thread = new Thread(DoASlowThing);
+			thread.Start();
+		}
+	}
+
+	public void DoASlowThing()
+	{
+		Debug.Log("Doing a thing");
+
+		for (int i = 0; i < 10000000; i++)
+		{
+			lock (aLock)
+			{
+				total += Mathf.PerlinNoise(i, 0);
+			}
+		}
+	
+		Debug.Log("Finished doing a thing : Total = "+total);
+	}
+}
