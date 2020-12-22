32186d9e0a766e1799799b0185a843a145a3ebd8
Slexx <slex-@hotmail.com>
2020-12-22 17:41:30 +1100

    started work on pathfinding script for learning outcomes

---
+++ Assets/Team members/AlexM/Scripts/WorldScanner.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlexM
+{
+
+	/// <summary>
+	/// What do i need?
+	/// Start.
+	/// End.
+	/// </summary>
+	public class Node
+	{
+		public bool isBlocked;
+		public bool isGoal;
+		public Vector3 myPos;
+	}
+	
+	public class WorldScanner : MonoBehaviour
+	{
+
+		public Node[,]   Nodes;
+		public int       xNum = 50;
+		public int       zNum = 50;
+		public LayerMask layerMask;
+		public LayerMask goalMask;
+
+		public void DrawGrid()
+		{
+			// Create the actual array
+			Nodes = new Node[xNum,zNum];
+		
+			for (int x = 0; x < xNum; x++)
+			{
+				for (int z = 0; z < zNum; z++)
+				{
+					Nodes[x,z] = new Node();
+				
+					if (Physics.CheckBox(transform.position + new Vector3(x, 0, z), new Vector3(0.8f, 0.8f, 0.8f), Quaternion.identity, layerMask.value))
+					{
+						Nodes[x, z].isBlocked = true;
+					}
+					
+					if (Physics.CheckBox(transform.position + new Vector3(x, 0, z), new Vector3(0.8f, 0.8f, 0.8f), Quaternion.identity, goalMask.value))
+					{
+						Nodes[x, z].isGoal = true;
+					}
+				}
+			}
+		}
+
+		void OnDrawGizmos()
+		{
+			
+			Vector3 transformPosition = transform.position;
+			Vector3 size              = new Vector3(0.8f, 0.8f, 0.8f);
+
+			
+			for (int x = 0; x < xNum; x++)
+			{
+				for (int z = 0; z < zNum; z++)
+				{
+					if (Nodes != null && !Nodes[x,z].isBlocked)
+					{
+						Gizmos.color = Color.green;
+						Gizmos.DrawCube(transformPosition + new Vector3(x, 0, z), size);
+					}
+					
+					if (Nodes != null && Nodes[x,z].isBlocked)
+					{
+						Gizmos.color = Color.red;
+						Gizmos.DrawCube(transformPosition + new Vector3(x, 0, z), size);
+					}
+
+					if (Nodes != null && Nodes[x,z].isGoal)
+					{
+						Gizmos.color = Color.yellow;
+						Gizmos.DrawCube(transformPosition + new Vector3(x, 0, z), size);
+					}
+				}
+			}
+		}
+	}
+}
