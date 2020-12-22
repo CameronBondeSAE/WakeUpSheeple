9e2a771e887cb00e622a45fde4599064da112a86
Riley McGowan <1017093@student.sae.edu.au>
2020-12-22 15:02:15 +1100

    Events and Curve Demo Scenes

---
+++ Assets/Team members/Riley/Scripts/Test Scripts/SubscribedObject1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SubscribedObject1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FindObjectOfType<buttonHandler>().buttonEvent += randomPos;
+    }
+    void randomPos()
+    {
+        transform.position = new Vector3(transform.position.x + Random.Range(-5, 5), transform.position.y, transform.position.z + Random.Range(-5, 5));
+    }
+    private void OnDisable()
+    {
+        FindObjectOfType<buttonHandler>().buttonEvent -= randomPos;
+    }
+}
---
+++ Assets/Team members/Riley/Scripts/Test Scripts/buttonHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonHandler : MonoBehaviour
+{
+    public event Action buttonEvent;
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 100, 20), "Event Button"))
+        {
+            buttonEvent?.Invoke();
+        }
+    }
+}
