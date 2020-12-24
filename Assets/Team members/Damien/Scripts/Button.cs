1ba66c194b95595f1cca97a6d65af3564c04abc2
Damien Harwood <1019243@student.sae.edu.au>
2020-12-24 16:52:41 +1100

    Button Finished

---
+++ Assets/Team members/Damien/Scripts/Button.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Damien
+
+{
+    public class Button : MonoBehaviour
+    {
+        public event Action actionButton;
+        public string buttonText;
+
+        public void OnGUI()
+        {
+            if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 100, 50), buttonText))
+            {
+                actionButton?.Invoke();
+            }
+        }
+    }
+}
---
+++ Assets/Team members/Damien/Scripts/SubscribedObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Damien;
+using NodeCanvas.Tasks.Actions;
+using UnityEngine;
+
+public class SubscribedObject : MonoBehaviour
+{
+    public float rangeToMove = 5f;
+    
+    
+    void Start()
+    {
+        FindObjectOfType<Button>().actionButton += randomPosition;
+    }
+    void randomPosition()
+    {
+        GetComponent<Renderer>().material.color =
+            new Color(Random.Range(10, 200), Random.Range(10, 200), Random.Range(10, 200), 200);
+        transform.position = new Vector3(transform.position.x + Random.Range(-rangeToMove, rangeToMove), transform.position.y + Random.Range(-rangeToMove, rangeToMove), transform.position.z + Random.Range(-rangeToMove, rangeToMove));
+        
+    }
+}
